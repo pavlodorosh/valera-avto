4c03831b870bc22488d8d74845b933e0fe1ad1a0
AmadeusDeadDmytro <mazurenkodmitriy57@gmail.com>
2020-08-09 17:54:52 +0300

    migrations

---
+++ database/migrations/2020_08_09_134829_create_type_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTypeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('type', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name', 150);
+            $table->string('image', 150);
+            $table->string('video', 150);
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('type');
+    }
+}
